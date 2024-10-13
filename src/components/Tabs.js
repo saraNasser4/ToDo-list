98c2d32a0c7b6e8bf2018017b1680015aca541c1
saraNasser4 <saransser6@gmail.com>
2024-10-13 08:56:24 +0300

    update TodoCard component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -13,7 +13,7 @@ function Tabs(props){
                 props.todos.filter(val => val.complete).length;
             return(
                 //dark:hover:border-b-sky-600
-                <button key={index} className={`p-4 mx-2 sm:text-xl font-medium hover:border-b-2  hover:border-b-sky-300 hover:pb-[14px] cursor-auto`}>
+                <button key={index} className={`p-4 mx-2 sm:text-xl lg:text-2xl font-medium hover:border-b-2  hover:border-b-sky-300 hover:pb-[14px] cursor-auto`}>
                     <h4 className='hover:text-sky-300 !cursor-pointer duration-200'>{tab} <span className='text-gray-400'>({numOfTasks})</span></h4>
                 </button>
             )
--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-function TodoCard () {
+//key={index} todoIndex={index} todo={todo}
+
+function TodoCard ({ todoIndex, todo }) {
+  const btnsMass =['Done', 'Delete']
   return (
-    <div>
-        TodoCard
+    <div className="bg-gray-200 mx-2 my-4 rounded-md p-2 font-[500] max-w-[650px]">
+      <p className='md:text-xl'>{todo.input}</p>
+      <div>
+        {btnsMass.map((btn, ind)=> {
+          return (
+            <button key={ind} className='mr-4 my-2 px-6 py-1.5 rounded-2xl first:bg-sky-300 text-white last:bg-purple-300'>
+              <h6>{btn}</h6>
+            </button>
+
+          )
+        })}
+      </div>
+        
     </div>
   )
 }
